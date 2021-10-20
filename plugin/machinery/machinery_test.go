803350d09d2448db2f030de80fc0a041124ba79a
xuzhiyong <xuzhiyong28>
2021-10-20 09:19:44 +0800

    提交代码。。。。

--- plugin/machinery/machinery_test.go
+++ plugin/machinery/machinery_test.go
@@ -3,7 +3,7 @@ package machinery
 import "github.com/RichardKnop/machinery/v1/config"
 
 /**
-	更多实例可以看 D:\webspack\goCode\pkg\mod\github.com\!richard!knop\machinery@v1.7.4\example\machinery.go
+更多实例可以看 D:\webspack\goCode\pkg\mod\github.com\!richard!knop\machinery@v1.7.4\example\machinery.go
 */
 
 import (
@@ -29,7 +29,7 @@ func startServer() (*machinery.Server, error) {
 	if err != nil {
 		return nil, err
 	}
-	server, err := machinery.NewServer(cnf)
+	server, err := machinery.NewServer(cnf) // 实例化
 	if err != nil {
 		return nil, err
 	}
@@ -47,6 +47,27 @@ func startServer() (*machinery.Server, error) {
 				sum *= arg
 			}
 			return sum, nil
+		},
+		"sum_ints": func(numbers []int64) (int64, error) {
+			var sum int64
+			for _, num := range numbers {
+				sum += num
+			}
+			return sum, nil
+		},
+		"sum_floats": func(numbers []float64) (float64, error) {
+			var sum float64
+			for _, num := range numbers {
+				sum += num
+			}
+			return sum, nil
+		},
+		"concat": func(strs []string) (string, error) {
+			var res string
+			for _, s := range strs {
+				res += s
+			}
+			return res, nil
 		},
 	}
 	return server, server.RegisterTasks(tasks)
@@ -68,8 +89,8 @@ func TestTask_Demo1(t *testing.T) {
 		fmt.Println("======= task - complete =======")
 	}()
 
-	// 发送任务
-	asyncResult, err := server.SendTaskWithContext(context.Background(), &tasks.Signature{
+	// 发送任务  - 1
+	asyncResult01, err := server.SendTaskWithContext(context.Background(), &tasks.Signature{
 		Name: "add",
 		Args: []tasks.Arg{
 			{
@@ -82,9 +103,48 @@ func TestTask_Demo1(t *testing.T) {
 			},
 		},
 	})
-	results, err := asyncResult.Get(time.Millisecond * 5)
+	results, err := asyncResult01.Get(time.Millisecond * 5)
 	if err != nil {
 		fmt.Errorf("Getting task result failed with error: %s", err.Error())
 	}
 	fmt.Printf("1 + 1 = %v\n", tasks.HumanReadableResults(results))
+}
+
+// 延迟队列
+func TestTaskDelay_Demo(t *testing.T) {
+	server, err := startServer()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	//启动一个工作队列
+	worker := server.NewWorker("asong", 1)
+	go func() {
+		err := worker.Launch()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println("======= task - complete =======")
+	}()
+	eta := time.Now().Add(time.Millisecond * 5)
+	// 指定一个延迟任务
+	signature :=&tasks.Signature{
+		Name: "add",
+		Args: []tasks.Arg{
+			{
+				Type:  "int64",
+				Value: 20,
+			},
+			{
+				Type:  "int64",
+				Value: 30,
+			},
+		},
+		RetryCount: 3, //重试次数
+		ETA: &eta,	//延迟秒数
+	}
+	_ , err = server.SendTask(signature)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	time.Sleep(5 * time.Minute)
 }
