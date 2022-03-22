d9f9ca7c20be07f74c74339c238c53992ca0c0d4
xuzhiyong <xuzhiyong28>
2022-03-22 13:52:53 +0800

    update

--- plugin/ants/go_ants_test.go
+++ plugin/ants/go_ants_test.go
@@ -1,7 +1,44 @@
 package ants
 
-import "testing"
+import (
+	"fmt"
+	"github.com/panjf2000/ants/v2"
+	"math/rand"
+	"sync"
+	"testing"
+)
+
+
+type task struct {
+	index int
+	wg    *sync.WaitGroup
+}
+
+func (t *task) Do() {
+	fmt.Println("任务编号 = " , t.index)
+	t.wg.Done()
+}
 
 func TestDemo1(t *testing.T) {
-	Demo1()
+	//创建一个容量为10的线程池
+	p, _ := ants.NewPoolWithFunc(10, func(data interface{}) {
+		t := data.(*task)
+		t.Do()
+	})
+
+	//构造数据
+	nums := make([]int, 10000, 10000)
+	for i := range nums {
+		nums[i] = rand.Intn(1000)
+	}
+	var wg sync.WaitGroup
+	wg.Add(100)
+	for i := 0 ; i < 100 ; i++ {
+		p.Invoke(&task{
+			index: i,
+			wg: &wg,
+		})
+	}
+	wg.Wait()
+	fmt.Printf("running goroutines: %d\n", ants.Running())
 }
