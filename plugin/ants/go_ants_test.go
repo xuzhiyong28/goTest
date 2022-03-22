369b57e282d094b2dcf13603345686c4e77ca331
xuzhiyong <xuzhiyong28>
2022-03-22 15:32:14 +0800

    提交代码。。。。

--- plugin/ants/go_ants_test.go
+++ plugin/ants/go_ants_test.go
@@ -3,11 +3,11 @@ package ants
 import (
 	"fmt"
 	"github.com/panjf2000/ants/v2"
-	"math/rand"
 	"sync"
+	"sync/atomic"
 	"testing"
+	"time"
 )
-
 
 type task struct {
 	index int
@@ -15,7 +15,7 @@ type task struct {
 }
 
 func (t *task) Do() {
-	fmt.Println("任务编号 = " , t.index)
+	fmt.Println("任务编号 = ", t.index)
 	t.wg.Done()
 }
 
@@ -26,19 +26,58 @@ func TestDemo1(t *testing.T) {
 		t.Do()
 	})
 
-	//构造数据
-	nums := make([]int, 10000, 10000)
-	for i := range nums {
-		nums[i] = rand.Intn(1000)
-	}
 	var wg sync.WaitGroup
 	wg.Add(100)
-	for i := 0 ; i < 100 ; i++ {
+	for i := 0; i < 100; i++ {
 		p.Invoke(&task{
 			index: i,
-			wg: &wg,
+			wg:    &wg,
 		})
 	}
 	wg.Wait()
 	fmt.Printf("running goroutines: %d\n", ants.Running())
+}
+
+// 官方测试案例1 ： https://github.com/panjf2000/ants/blob/master/README_ZH.md
+// 使用的是ants自带的默认线程池 defaultAntsPool,看了下 容量 = 2147483647
+func TestDemo0(t *testing.T) {
+	defer ants.Release()
+	runTimes := 1000
+	var wg sync.WaitGroup
+	syncCalculateSum := func() {
+		time.Sleep(10 * time.Millisecond)
+		fmt.Println("Hello World!")
+		wg.Done()
+	}
+	for i := 0; i < runTimes; i++ {
+		wg.Add(1)
+		ants.Submit(syncCalculateSum)
+	}
+	wg.Wait()
+	fmt.Printf("running goroutines: %d\n", ants.Running())
+	fmt.Printf("finish all tasks.\n")
+}
+
+// 官方测试案例2 ： https://github.com/panjf2000/ants/blob/master/README_ZH.md
+func TestDemo2(t *testing.T) {
+	var sum int32
+	var wg sync.WaitGroup
+	runTimes := 1000
+	myFunc := func(i interface{}) {
+		n := i.(int32)
+		atomic.AddInt32(&sum, n)
+		fmt.Printf("run with %d\n", n)
+	}
+	p, _ := ants.NewPoolWithFunc(10, func(i interface{}) {
+		myFunc(i)
+		wg.Done()
+	})
+	defer p.Release()
+	for i := 0; i < runTimes; i++ {
+		wg.Add(1)
+		_ = p.Invoke(int32(i))
+	}
+	wg.Wait()
+	fmt.Printf("running goroutines: %d\n", p.Running())
+	fmt.Printf("finish all tasks, result is %d\n", sum)
 }
